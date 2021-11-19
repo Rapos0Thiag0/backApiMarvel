0d08ecdc36aeee2dd1e38ccf26b952a77f280f1d
lucasmortola <lucasmortolait@outlook.com>
2021-11-18 22:29:44 -0300

    commit alterações gitignore e json

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import express,{Request,Response,NextFunction} from 'express'
+import cors from 'cors'; // previnir erro no heroko
+import axios from 'axios'; // Requisiçoes 
+import md5 from 'md5';
+import 'dotenv/config'
+//Chave Marvel
+const publicKey = 'b60115b9e7a50dd36c8c4cd76c94be33';
+const privateKey = 'ea81bf0a9de7ae4eb4e441bcbb8c2e46f896aa40';
+const  urlApi = 'http://gateway.marvel.com/v1/public';
+
+
+const app = express();
+
+
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port,()=>{
+    console.log("Servidor executando na porta " + port)
+})
+
+
+app.get("/", (req:Request,res:Response,next:NextFunction)=>{
+    //limite nao pode ser menor que 1 ou maior que 100
+    //pagina 
+    const {page,limit} = req.query; //vai receber a pagina atual e o limite de exibição
+
+    const ts = new Date().getTime().toString();
+    const hash = md5(ts + privateKey + publicKey)
+
+    axios.get(`${urlApi}/characters`,{
+        params:{
+            ts:ts,
+            apikey: publicKey,
+            hash: hash,
+            orderBy: 'name',
+            limit: 10,
+            offset: Number(limit) * Number(page)
+        }
+    }).then((response => {
+        res.json(response.data);
+    })).catch(err =>{
+        res.status(500).send('erro interno');
+    })
+})
+
+
